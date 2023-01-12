e56500605bdec0c7439a38a78494e7aa87931ccd
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2023-01-11 20:30:10 -0500

    Update 178. Rank Scores.sql

--- 178. Rank Scores.sql
+++ 178. Rank Scores.sql
@@ -1,6 +1,16 @@
+#Solution 1
 # It's wired that I have to add '' after add to make it work now
 # Dense_RANK 1 2 2 3
 # RANK 1 2 2 4
 # ROW_RANK 1 2 3 4
 SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank'
 FROM Scores
+
+#Solution 2
+# Write your MySQL query statement below
+SELECT S1.score, 
+    (SELECT COUNT(DISTINCT score)
+    FROM Scores S2
+    WHERE S1.score <= S2.Score) AS 'rank'
+FROM Scores S1
+ORDER BY Score DESC
