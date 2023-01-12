9c33d617a9c99cf25fcdcae0e3a6ec3323bf2e40
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2023-01-12 11:06:38 -0500

    Update 183. Customers Who Never Order.sql

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -7,7 +7,7 @@ C.id =  O.customerID
 WHERE O.id is NULL
 
 #Solution 2
-Does't work if 2 customers have the same name, so use id instead...
+#Does't work if 2 customers have the same name, so use id instead...
 # Write your MySQL query statement below
 SELECT name AS Customers 
 FROM Customers
