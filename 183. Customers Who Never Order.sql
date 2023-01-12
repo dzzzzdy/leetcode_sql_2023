a97223a992bce334572bc52d574de3ad5a1ddd3f
dzzzzdy <96793981+dzzzzdy@users.noreply.github.com>
2023-01-12 11:05:27 -0500

    Create 183. Customers Who Never Order.sql

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,18 @@
+#Solution 1
+# Write your MySQL query statement below
+SELECT name AS Customers
+FROM Customers C
+LEFT JOIN Orders O ON
+C.id =  O.customerID
+WHERE O.id is NULL
+
+#Solution 2
+Does't work if 2 customers have the same name
+# Write your MySQL query statement below
+SELECT name AS Customers 
+FROM Customers
+WHERE name NOT IN(
+    SELECT name 
+    FROM Customers C
+    JOIN Orders O ON
+    C.id =  O.customerID)
